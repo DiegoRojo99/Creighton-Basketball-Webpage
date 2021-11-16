3c7f6e0c277b32b80358ec542027f84cb4019f8f
DiegoRojo99 <diego99rojo@gmail.com>
2021-11-16 17:24:44 -0600

    Schedule page with header and footer

---
+++ schedule.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Creighton Men's Basketball</title>
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <header>
+      <div class="header-title">
+        <img class="creighton-image" src="creighton.png" />
+        <img class="basketball-image" src="basketball.png" />
+        <h1>Creighton Men's Basketball</h1>
+      </div>
+      <nav class="header-nav">
+        <ul>
+          <li><a href="index.php" id="this">Home</a></li>
+          <li><a href="roster.php">Roster</a></li>
+          <li><a href="schedule.php">Schedule</a></li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      <section>
+        
+      </section>
+
+    </main>
+    <footer>
+      <div>
+        Page made by Diego Rojo
+        <a href="https://github.com/DiegoRojo99" title="Github">Github</a> 
+      </div>
+    </footer>
+  </body>
+</html>
